4fee9ffb9ea12e4c3975ff1d7484602bb5c239cb
rutledka <khalilrutledge@gmail.com>
2016-10-27 09:51:35 -0400

    pulling in post

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const snoowrap = require('snoowrap');
 const _ = require('lodash');
 const api = require('./client');
 
+
 /*
 * Constants
 */
 const _VER_ = '0.1.0';
 const subreddit = '/r/noveltranslations';
+const config = {
+  userAgent : '/u/lordfirst LordFirstApp@' + _VER_ + ' SEVENTH MOUNTAIN AND SEA',
+  clientId : api.id,
+  clientSecret : api.secretKey,
+  username: api.username,
+  password: api.password
+}
 
 // Our snoocore instantiation
 const reddit = new snoowrap({
-  userAgent: '/u/lordfirst LordFirstApp@' + _VER_ + 'SEVENTH MOUNTAIN AND SEA', // unique string identifying the app
-  clientId: api.id, // OAuth client key (provided at reddit app)
-  clientSecret: api.secretKey, // OAuth secret (provided at reddit app)
-  username: api.username, // Reddit username used to make the reddit app
-  password: api.password, // Reddit password for the username
+  userAgent: config.userAgent, // unique string identifying the app
+  clientId: config.clientId, // OAuth client key (provided at reddit app)
+  clientSecret: config.clientSecret, // OAuth secret (provided at reddit app)
+  username: config.username, // Reddit username used to make the reddit app
+  password: config.password // Reddit password for the username
   // The OAuth scopes that we need to make the calls that we
   // want. The reddit documentation will specify which scope
   // is needed for evey call
